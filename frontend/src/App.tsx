c4d17b4db17b1b47fbb772d9762c81edbe53f7cb
VENKATESH <vchary78@gmail.com>
2026-08-19 07:30:28 +0530

    comment un used code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <section id="center" className='text-2xl bg-amber-200'>
-        <h1> hiii</h1>
+      <section className='bg-slate-900 text-white max-w-4xl text-center space-y-2 border-cyan-900 shadow-2xl mx-auto p-2'>
+        <h1 className='font-bold text-3xl'> Lets learn Tailwind!</h1>
+        <p className="loren">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti saepe dolore tenetur, perspiciatis explicabo, temporibus cupiditate minima veniam ad adipisci laudantium enim numquam amet velit repellendus pariatur quod? Beatae, perspiciatis.
+        </p>
+        <button className='border-amber-300 text-red-400 p-1'>submit</button>
       </section>
     </>
   )
